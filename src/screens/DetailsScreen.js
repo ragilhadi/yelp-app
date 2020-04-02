b26281c38eba47ec9017faccd746bc3f55dc9741
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-02 22:53:25 +0700

    first commit

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Detal Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, StyleSheet, Button, View } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+  return (
+      <View>
+            <Text style={styles.text}>HomeScreen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details')}
+            />
+      </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
