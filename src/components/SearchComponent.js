fad025faa7e3cd307012e22bd710e941bef61619
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-03 20:38:11 +0700

    Add Yelp Api with Axios

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -4,7 +4,7 @@ import { Feather } from '@expo/vector-icons'
 
 const SearchComponent = ({term,changeHandling,onSubmit}) => {
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
         <Feather name="search" style={styles.icon}/>
         <TextInput 
             value={term}
@@ -22,16 +22,22 @@ const SearchComponent = ({term,changeHandling,onSubmit}) => {
 const styles = StyleSheet.create({
     container : {
         flexDirection: 'row',
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 10,
+        marginHorizontal: 24,
+        marginTop: 16
     },
     icon: {
-        fontSize: 26
+        fontSize: 26,
+        alignSelf: 'center',
+        marginLeft: 16
     },
     input: {
-      borderWidth: 2,
-      borderColor: 'black',
       fontSize: 16,
       paddingHorizontal: 16,
-      paddingVertical: 8
+      paddingVertical: 8,
+      flex: 1
     }
   });
   
